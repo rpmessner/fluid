6508e8137eb1ab3c05a5484b39358ffcfd17f03d
rpmessner <rpmessner@gmail.com>
2013-05-19 21:54:38 -0500

    remove wrongly named file

--- lib/fluid/bocks.ex
+++
@@ -1,7 +0,0 @@
-defmodule Fluid.Blocks do
-  def create(markup) do
-    [name|rest] = String.split(markup, " ")
-    name = binary_to_atom(name, :utf8)
-    Fluid.Block[name: name, markup: Enum.join(rest, " ")]
-  end
-end
