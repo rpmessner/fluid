0d14350b12249673c14c9f4476fd0ed152e00d49
peter <peter.arentsen@outlook.com>
2015-12-28 00:23:27 +0100

    Update version to latest version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Liquid.Mixfile do
 
   def project do
     [ app: :liquid,
-      version: "0.1",
+      version: "0.1.0",
       elixir: "~> 1.0",
       deps: deps,
       name: "Liquid",
