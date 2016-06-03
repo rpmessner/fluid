fbfd342e4a7466e946dd96198a9a568dcc64393b
Peter Arentsen <peter.arentsen@quicknet.nl>
2016-06-04 01:06:47 +0200

    Bump version number for new release

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Liquid.Mixfile do
 
   def project do
     [ app: :liquid,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.0",
       deps: deps,
       name: "Liquid",
