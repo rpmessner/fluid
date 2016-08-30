df7c7e6999698286e2bc5729afda3414a7de0176
Vladimir Kravchenko <art4ride@gmail.com>
2016-08-31 00:49:28 +0700

    nulian/liquid-elixir#6 New filters: reverse, sort, uniq, join, truncate, truncatewords, slice, escape, h, escape_once, strip_html

--- lib/liquid/filters.ex
+++ lib/liquid/filters.ex
@@ -1,4 +1,5 @@
 defmodule Liquid.Filters do
+  alias Liquid.HTML
 
   defmodule Functions do
     def size(input) when is_binary(input) do
@@ -13,7 +14,7 @@ defmodule Liquid.Filters do
       tuple_size(input)
     end
 
-    def size(input), do: 0
+    def size(_), do: 0
 
     def downcase(input) do
       input |> to_string |> String.downcase
@@ -25,6 +26,62 @@ defmodule Liquid.Filters do
 
     def capitalize(input) do
       input |> to_string |> String.capitalize
+    end
+
+    def reverse(array), do: array |> to_iterable |> Enum.reverse
+
+    def sort(array), do: array |> Enum.sort
+
+    def sort(array, key) do
+      case array do
+        [head|_] when is_map(head) -> array |> Enum.sort_by(&(&1[key]))
+        _ -> array |> Enum.sort
+      end
+    end
+
+    def uniq(array), do: array |> Enum.uniq
+
+    def uniq(array, key) do
+      case array do
+        [head|tail] when is_map(head) -> [head|tail] |> Enum.uniq_by(&(&1[key]))
+        array -> array |> Enum.uniq
+      end
+    end
+
+    def join(array, separator \\ " ") do
+      array |> to_iterable |> Enum.join(separator)
+    end
+
+    def truncate(input, l \\ 50)
+
+    def truncate(nil, _), do: nil
+
+    def truncate(input, l) do
+      truncate_string = "..."
+      l = l - String.length(truncate_string) - 1
+      case {l, String.length(input)} do
+        {l, _} when l <= 0 -> truncate_string
+        {l, len} when l < len -> String.slice(input,0..l) <> truncate_string
+        _ -> input
+      end
+    end
+    
+    def truncatewords(input, words \\ 15)
+
+    def truncatewords(nil, _), do: nil
+
+    def truncatewords(input, words) do
+      truncate_string = "..."
+      wordlist = input |> String.split(" ")
+
+      l = words - 1
+      case l do
+        l when l < 0 -> wordlist[0]
+        l when l < length(wordlist) ->
+          words = wordlist |> Enum.slice(0..l) |> Enum.join(" ") 
+          words <> truncate_string
+        _ -> input
+      end
     end
 
 
@@ -43,13 +100,79 @@ defmodule Liquid.Filters do
       String.split(string, separator)
     end
 
-    def split(nil, <<separator::binary>>), do: []
+    def split(nil, _), do: []
+
+    def slice(list, from, to) when is_list(list) do
+      list |> Enum.slice(from, to)
+    end
+
+    def slice(<<string::binary>>, from, to) do
+      string |> String.slice(from, to)
+    end
+
+    def slice(list, range) when is_list(list) do
+      len = length(list)
+      case range do
+        0 -> list
+        range when range > 0 -> list |> Enum.slice(range, len)
+        range -> list |> Enum.slice(len + range, len)
+      end
+    end
+
+    def slice(<<string::binary>>, range) do
+      len = String.length(string)
+      case range do
+        0 -> string
+        range when range > 0 -> string |> String.slice(range, len)
+        range -> string |> String.slice(len + range, len)
+      end
+    end
+
+
+    def slice(nil, _), do: ""
+
+    def escape(input) when is_binary(input) do
+      input |> HTML.html_escape
+    end
+
+    defdelegate h(input), to: __MODULE__, as: :escape
+
+    def escape_once(input) when is_binary(input) do
+      input |> HTML.html_escape_once
+    end
+
+    def strip_html(nil), do: ""
+    
+    def strip_html(input) when is_binary(input) do
+      input
+        |> String.replace(~r/<script.*?<\/script>/m, "") 
+        |> String.replace(~r/<!--.*?-->/m, "")
+        |> String.replace(~r/<style.*?<\/style>/m, "")
+        |> String.replace(~r/<.*?>/m, "")
+    end
 
     def url_encode(input) when is_binary(input) do
       input |> URI.encode_www_form
     end
 
     def url_encode(nil), do: nil
+
+    defp to_iterable(input) do
+      case input do
+        input when is_list(input) ->
+          case List.first(input) do
+            first when is_number(first) ->
+              input |> List.flatten
+            first when is_nil(first) ->
+              []
+            _ -> [input]
+          end
+          
+        input when is_map(input) -> [input]
+        # input when is_tuple(input) -> input 
+        input -> List.wrap(input)
+      end
+    end
 
   end
 
--- test/liquid/filter_test.exs
+++ test/liquid/filter_test.exs
@@ -24,7 +24,6 @@ defmodule Liquid.FilterTest do
     assert "'barbar'" == Filters.filter( filters, name)
   end
 
-
   test :size do
     assert 3 == Functions.size([1,2,3])
     assert 0 == Functions.size([])
@@ -58,15 +57,123 @@ defmodule Liquid.FilterTest do
   end
 
 
+  test :slice do
+    assert "oob" == Functions.slice("foobar", 1, 3)
+    assert "oobar" == Functions.slice("foobar", 1, 1000)
+    assert "" == Functions.slice("foobar", 1, 0)
+    assert "o" == Functions.slice("foobar", 1, 1)
+    assert "bar" == Functions.slice("foobar", 3, 3)
+    assert "ar" == Functions.slice("foobar", -2, 2)
+    assert "ar" == Functions.slice("foobar", -2, 1000)
+    assert "r" == Functions.slice("foobar", -1)
+    assert "" == Functions.slice(nil, 0)
+    assert "" == Functions.slice("foobar", 100, 10)
+    assert "" == Functions.slice("foobar", -100, 10)
+  end
+
+  test :slice_on_arrays do
+    input = "foobar" |> String.split("")
+    input = input |> List.delete("")
+    assert ~w{o o b} == Functions.slice(input, 1, 3)
+    assert ~w{o o b a r} == Functions.slice(input, 1, 1000)
+    assert ~w{} == Functions.slice(input, 1, 0)
+    assert ~w{o} == Functions.slice(input, 1, 1)
+    assert ~w{b a r} == Functions.slice(input, 3, 3)
+    assert ~w{a r} == Functions.slice(input, -2, 2)
+    assert ~w{a r} == Functions.slice(input, -2, 1000)
+    assert ~w{r} == Functions.slice(input, -1)
+    assert ~w{} == Functions.slice(input, 100, 10)
+    assert ~w{} == Functions.slice(input, -100, 10)
+  end
+
+  test :truncate do
+    assert "1234..." == Functions.truncate("1234567890", 7)
+    assert "1234567890" == Functions.truncate("1234567890", 20)
+    assert "..." == Functions.truncate("1234567890", 0)
+    assert "1234567890" == Functions.truncate("1234567890")
+    assert "测试..." == Functions.truncate("测试测试测试测试", 5)
+  end
+
+
   test :split do
     assert ["12","34"] == Functions.split("12~34", "~")
     assert ["A? "," ,Z"] == Functions.split("A? ~ ~ ~ ,Z", "~ ~ ~")
     assert ["A?Z"] == Functions.split("A?Z", "~")
+    # Regexp works although Liquid does not support.
+    # assert ["A","Z"] == Functions.split("AxZ", ~r/x/)
     assert [] == Functions.split(nil, " ")
+  end
+
+
+  test :escape do
+    assert "&lt;strong&gt;" == Functions.escape("<strong>")
+    assert "&lt;strong&gt;" == Functions.h("<strong>")
+  end
+
+  test :escape_once do
+    assert "&lt;strong&gt;Hulk&lt;/strong&gt;" == Functions.escape_once("&lt;strong&gt;Hulk</strong>")
   end
 
   test :url_encode do
     assert "foo%2B1%40example.com" == Functions.url_encode("foo+1@example.com")
     assert nil == Functions.url_encode(nil)
   end
+
+  test :truncatewords do
+    assert "one two three" == Functions.truncatewords("one two three", 4)
+    assert "one two..." == Functions.truncatewords("one two three", 2)
+    assert "one two three" == Functions.truncatewords("one two three")
+    assert "Two small (13&#8221; x 5.5&#8221; x 10&#8221; high) baskets fit inside one large basket (13&#8221;..." == Functions.truncatewords("Two small (13&#8221; x 5.5&#8221; x 10&#8221; high) baskets fit inside one large basket (13&#8221; x 16&#8221; x 10.5&#8221; high) with cover.", 15)
+    assert "测试测试测试测试" == Functions.truncatewords("测试测试测试测试", 5)
+  end
+
+  test :strip_html do
+    assert "test" == Functions.strip_html("<div>test</div>")
+    assert "test" == Functions.strip_html(~S{<div id="test">test</div>})
+    assert "" == Functions.strip_html(~S{<script type="text/javascript">document.write("some stuff");</script>})
+    assert "" == Functions.strip_html(~S{<style type="text/css">foo bar</style>})
+    assert "test" == Functions.strip_html(~S{<div\nclass="multiline">test</div>})
+    assert "test" == Functions.strip_html(~S{<!-- foo bar \n test -->test})
+    assert "" == Functions.strip_html(nil)
+  end
+
+  test :join do
+    assert "1 2 3 4" == Functions.join([1, 2, 3, 4])
+    assert "1 - 2 - 3 - 4" == Functions.join([1, 2, 3, 4], " - ")
+  end
+
+  test :sort do
+    assert [1, 2, 3, 4] == Functions.sort([4, 3, 2, 1])
+    assert [%{"a" => 1}, %{"a" => 2}, %{"a" => 3}, %{"a" => 4}] == Functions.sort([%{"a" => 4}, %{"a" => 3}, %{"a" => 1}, %{"a" => 2}], "a")
+    assert [%{"a" => 1, "b" => 1}, %{"a" => 3, "b" => 2}, %{"a" => 2, "b"=> 3}] == Functions.sort([%{"a" => 3, "b" => 2}, %{"a" => 1, "b" => 1}, %{"a" => 2, "b"=> 3}], "b")
+    # Elixir keyword list support
+    assert ["a": 1, "a": 2, "a": 3, "a": 4] == Functions.sort([{:a, 4}, {:a, 3}, {:a, 1}, {:a, 2}], "a")
+  end
+
+  test :legacy_sort_hash do
+    assert Map.to_list(%{a: 1, b: 2}) == Functions.sort(a: 1, b: 2)
+  end
+
+  test :numerical_vs_lexicographical_sort do
+    assert [2, 10] == Functions.sort([10, 2])
+    assert [{"a", 2}, {"a", 10}] == Functions.sort([{"a", 10}, {"a", 2}], "a")
+    assert ["10", "2"] == Functions.sort(["10", "2"])
+    assert [{"a", "10"}, {"a", "2"}] == Functions.sort([{"a", "10"}, {"a", "2"}], "a")
+  end
+
+  test :uniq do
+    assert [1, 3, 2, 4] == Functions.uniq([1, 1, 3, 2, 3, 1, 4, 3, 2, 1])
+    assert [{"a", 1}, {"a", 3}, {"a", 2}] == Functions.uniq([{"a", 1}, {"a", 3}, {"a", 1}, {"a", 2}], "a")
+    # testdrop = TestDrop.new
+    # assert [testdrop] == Functions.uniq([testdrop, TestDrop.new], "test")
+  end
+
+  test :reverse do
+    assert [4, 3, 2, 1] == Functions.reverse([1, 2, 3, 4])
+  end
+
+  test :legacy_reverse_hash do
+    assert [Map.to_list(%{a: 1, b: 2})] == Functions.reverse(a: 1, b: 2)
+  end
+
 end
