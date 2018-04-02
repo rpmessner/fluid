a13e31eaca6e09e829c25abe2ce1fafe0b4b5a1c
Antonio Abella <antonioabella@designiostecnologicos.com>
2018-04-02 19:43:45 -0400

    doc: documentation for IfElse tag

--- lib/liquid/tags/if_else.ex
+++ lib/liquid/tags/if_else.ex
@@ -1,46 +1,53 @@
 defmodule Liquid.ElseIf do
-  def parse(%Liquid.Tag{}=tag, %Liquid.Template{}=t), do: { tag, t }
+  def parse(%Liquid.Tag{} = tag, %Liquid.Template{} = t), do: {tag, t}
   def render(_, _, _, _), do: raise "should never get here"
 end
 
 defmodule Liquid.Else do
-  def parse(%Liquid.Tag{}=tag, %Liquid.Template{}=t), do: { tag, t }
+  def parse(%Liquid.Tag{} = tag, %Liquid.Template{} = t), do: {tag, t}
   def render(_, _, _, _), do: raise "should never get here"
 end
 
 defmodule Liquid.IfElse do
-  alias Liquid.Condition
-  alias Liquid.Render
-  alias Liquid.Block
-  alias Liquid.Tag
-  alias Liquid.Template
+  @moduledoc """
+   If is the conditional block
+   ```
+     {% if user.admin %}
+       Admin user!
+     {% else %}
+       Not admin user
+     {% endif %}
+      There are {% if count < 5 %} less {% else %} more {% endif %} items than you need.
+  ```
+  """
+  alias Liquid.{Block, Condition, Render, Tag, Template}
 
   def syntax, do: ~r/(#{Liquid.quoted_fragment})\s*([=!<>a-z_]+)?\s*(#{Liquid.quoted_fragment})?/
   def expressions_and_operators do
     ~r/(?:\b(?:\s?and\s?|\s?or\s?)\b|(?:\s*(?!\b(?:\s?and\s?|\s?or\s?)\b)(?:#{Liquid.quoted_fragment}|\S+)\s*)+)/
   end
 
-  def parse(%Block{}=block, %Template{}=t) do
+  def parse(%Block{} = block, %Template{} = t) do
     block = parse_conditions(block)
     case Block.split(block, [:else, :elsif]) do
-      { true_block, [%Tag{name: :elsif, markup: markup}|elsif_block] } ->
-        { elseif, t } =
+      {true_block, [%Tag{name: :elsif, markup: markup}|elsif_block]} ->
+        {elseif, t} =
           parse(%Block{name: :if, markup: markup, nodelist: elsif_block, blank: Blank.blank?(elsif_block)}, t)
-        { %{block | nodelist: true_block, elselist: [elseif], blank: Blank.blank?(true_block) }, t }
-      { true_block, [%Tag{name: :else}|false_block] } ->
+        {%{block | nodelist: true_block, elselist: [elseif], blank: Blank.blank?(true_block)}, t}
+      {true_block, [%Tag{name: :else}|false_block]} ->
         blank? = Blank.blank?(true_block) && Blank.blank?(false_block)
-        { %{block | nodelist: true_block, elselist: false_block, blank: blank?}, t }
-      { _, [] } ->
-        { %{block | blank: Blank.blank?(block.nodelist)}, t }
+        {%{block | nodelist: true_block, elselist: false_block, blank: blank?}, t}
+      {_, []} ->
+        {%{block | blank: Blank.blank?(block.nodelist)}, t}
     end
   end
 
   def render(output, %Tag{}, context) do
-    { output, context }
+    {output, context}
   end
 
   def render(output, %Block{blank: true} = block, context) do
-    {_, context} = render(output, %{block | blank: false }, context)
+    {_, context} = render(output, %{block | blank: false}, context)
     {output, context}
   end
 
@@ -53,16 +60,17 @@ defmodule Liquid.IfElse do
   defp split_conditions(expressions) do
     expressions |> List.flatten |> Enum.map(&String.trim/1) |> Enum.map(fn(x) ->
       case syntax() |> Regex.scan(x) do
-        [[_, left, operator, right]] -> { left, operator, right }
+        [[_, left, operator, right]] -> {left, operator, right}
         [[_, x]] -> x
+        _ -> raise Liquid.SyntaxError, message: "Check the parenthesis"
       end
     end)
   end
 
-  defp parse_conditions(%Block{markup: markup}=block) do
+  defp parse_conditions(%Block{markup: markup} = block) do
     expressions = Regex.scan(expressions_and_operators(), markup)
     expressions = expressions |> split_conditions |> Enum.reverse
     condition   = Condition.create(expressions)
-    %{block | condition: condition }
+    %{block | condition: condition}
   end
 end
