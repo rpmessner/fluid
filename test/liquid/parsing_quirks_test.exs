8964b66ba03bbffeefc239bffb0f4348b171b181
Antonio Abella <antonioabella@designiostecnologicos.com>
2018-04-02 19:43:08 -0400

    test: raise SyntaxError when bad formed `if` tag

--- test/liquid/parsing_quirks_test.exs
+++ test/liquid/parsing_quirks_test.exs
@@ -6,4 +6,14 @@ defmodule Liquid.ParsingQuirksTest do
   test "error on empty filter" do
     assert_raise(SyntaxError, fn -> Template.parse("{{|test}}") end)
   end
+
+  test "meaningless parens error" do
+    markup = "a == 'foo' or (b == 'bar' and c == 'baz') or false"
+    assert_raise(SyntaxError, fn -> Template.parse("{% if #{markup} %} YES {% endif %}") end)
+  end
+
+  test "unexpected characters syntax error" do
+    markup = "true && false"
+    assert_raise(SyntaxError, fn -> Template.parse("{% if #{markup} %} YES {% endif %}") end)
+  end
 end
