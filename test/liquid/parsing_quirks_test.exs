879df5176f7bba88bc32edf9b7091e9a120fa076
Antonio Abella <antonioabella@designiostecnologicos.com>
2018-04-02 20:10:12 -0400

    refactor: assert_syntax_error

--- test/liquid/parsing_quirks_test.exs
+++ test/liquid/parsing_quirks_test.exs
@@ -4,16 +4,19 @@ defmodule Liquid.ParsingQuirksTest do
   alias Liquid.{Template, SyntaxError}
 
   test "error on empty filter" do
-    assert_raise(SyntaxError, fn -> Template.parse("{{|test}}") end)
+    assert_syntax_error("{{|test}}")
   end
 
   test "meaningless parens error" do
     markup = "a == 'foo' or (b == 'bar' and c == 'baz') or false"
-    assert_raise(SyntaxError, fn -> Template.parse("{% if #{markup} %} YES {% endif %}") end)
+    assert_syntax_error("{% if #{markup} %} YES {% endif %}")
   end
 
   test "unexpected characters syntax error" do
     markup = "true && false"
-    assert_raise(SyntaxError, fn -> Template.parse("{% if #{markup} %} YES {% endif %}") end)
+    assert_syntax_error("{% if #{markup} %} YES {% endif %}")
+  end
+  defp assert_syntax_error(markup) do
+    assert_raise(SyntaxError, fn -> Template.parse(markup) end)
   end
 end
