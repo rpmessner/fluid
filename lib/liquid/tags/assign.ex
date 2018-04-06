22b2a2661398d3cd7db23f9d5f2c3d61a7229fb5
Antonio Abella <antonioabella@designiostecnologicos.com>
2018-04-05 20:31:40 -0400

    refactor: clean assign tag code

--- lib/liquid/tags/assign.ex
+++ lib/liquid/tags/assign.ex
@@ -1,18 +1,20 @@
 defmodule Liquid.Assign do
-  alias Liquid.Variable
-  alias Liquid.Tag
-  alias Liquid.Context
+  alias Liquid.{Context, Tag, Variable}
 
   def syntax, do: ~r/([\w\-]+)\s*=\s*(.*)\s*/
 
-  def parse(%Tag{}=tag, %Liquid.Template{}=template), do: { %{tag | blank: true }, template }
+  def parse(%Tag{} = tag, %Liquid.Template{} = template), do: {%{tag | blank: true}, template}
 
-  def render(output, %Tag{markup: markup}, %Context{}=context) do
+  def render(output, %Tag{markup: markup}, %Context{} = context) do
     [[_, to, from]] = syntax() |> Regex.scan(markup)
-    from_value = from |> Variable.create
-                      |> Variable.lookup(context)
+
+    from_value =
+      from
+      |> Variable.create()
+      |> Variable.lookup(context)
+
     result_assign = context.assigns |> Map.put(to, from_value)
     context = %{context | assigns: result_assign}
-    { output, context }
+    {output, context}
   end
 end
