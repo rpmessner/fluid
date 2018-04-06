f65552be57417175108204666dd2a8b92e263123
ingsissam <33227372+ingsissam@users.noreply.github.com>
2018-04-06 16:59:47 -0400

    doc: adding basic doc to the lib

--- lib/liquid/context.ex
+++ lib/liquid/context.ex
@@ -1,7 +1,22 @@
 defmodule Liquid.Context do
-  defstruct assigns: %{}, offsets: %{}, registers: %{}, presets: %{}, blocks: [],
-            extended: false, continue: false, break: false, template: nil, global_filter: nil, extra_tags: %{}
+  @moduledoc """
+  This module creates a structure `(Structs)` that gives the context for rendering
+  """
+  defstruct assigns: %{},
+            offsets: %{},
+            registers: %{},
+            presets: %{},
+            blocks: [],
+            extended: false,
+            continue: false,
+            break: false,
+            template: nil,
+            global_filter: nil,
+            extra_tags: %{}
 
+  @doc """
+  it takes the context `Struct` and get the selectet key assosiated value
+  """
   def registers(context, key) do
     context.registers |> Map.get(key)
   end
--- lib/liquid/expression.ex
+++ lib/liquid/expression.ex
@@ -1,35 +1,68 @@
 defmodule Liquid.Expression do
+  @moduledoc """
+  This module transform strings to  expresions 
+  """
   alias Liquid.Variable
   alias Liquid.RangeLookup
 
   @literal_list [nil, "nil", "null", "", "true", "false", "blank", "empty"]
   @literals %{
-    :nil => nil, "nil" => nil, "null" => nil, "" => nil,
+    nil => nil,
+    "nil" => nil,
+    "null" => nil,
+    "" => nil,
     "true" => true,
     "false" => false,
     "blank" => :blank?,
     "empty" => :empty?
   }
 
+  @doc """
+  This function takes a markup `(string)`, if the string is in the expression list transforms it in a valid expression, if not creates a variable struct `Liquid.Variable.create(markup)`
+
+  ##Example
+  ```
+  iex(1)> Liquid.Expression.parse("true")
+  true
+
+  iex(2)> Liquid.Expression.parse("hello")
+  %Liquid.Variable{filters: [], literal: nil, name: "hello", parts: ["hello"]}
+  ```
+  """
+
   def parse(markup) when markup in @literal_list, do: @literals[markup]
+
   def parse(markup) do
     cond do
-      Regex.match?(~r/\A'(.*)'\z/m, markup) -> # Single quoted strings
+      # Single quoted strings
+      Regex.match?(~r/\A'(.*)'\z/m, markup) ->
         [result] = Regex.run(~r/\A'(.*)'\z/m, markup, capture: :all_but_first)
         result
-      Regex.match?(~r/\A"(.*)"\z/m, markup) ->  # Double quoted strings
+
+      # Double quoted strings
+      Regex.match?(~r/\A"(.*)"\z/m, markup) ->
         [result] = Regex.run(~r/\A"(.*)"\z/m, markup, capture: :all_but_first)
         result
-      Regex.match?(~r/\A(-?\d+)\z/, markup) ->  # Integer and floats
+
+      # Integer and floats
+      Regex.match?(~r/\A(-?\d+)\z/, markup) ->
         [result] = Regex.run(~r/\A(-?\d+)\z/, markup, capture: :all_but_first)
         String.to_integer(result)
-      Regex.match?(~r/\A\((\S+)\.\.(\S+)\)\z/, markup) -> # Ranges
-        [left_range, right_range] = Regex.run(~r/\A\((\S+)\.\.(\S+)\)\z/, markup, capture: :all_but_first)
+
+      # Ranges
+      Regex.match?(~r/\A\((\S+)\.\.(\S+)\)\z/, markup) ->
+        [left_range, right_range] =
+          Regex.run(~r/\A\((\S+)\.\.(\S+)\)\z/, markup, capture: :all_but_first)
+
         RangeLookup.parse(left_range, right_range)
-      Regex.match?(~r/\A(-?\d[\d\.]+)\z/, markup) -> # Floats
+
+      # Floats
+      Regex.match?(~r/\A(-?\d[\d\.]+)\z/, markup) ->
         [result] = Regex.run(~r/\A(-?\d[\d\.]+)\z/, markup, capture: :all_but_first)
         result
-      true -> Variable.create(markup)
+
+      true ->
+        Variable.create(markup)
     end
   end
 end
