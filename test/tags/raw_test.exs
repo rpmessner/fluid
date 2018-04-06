323cb7a7d1dedfeb1ccc2d399b990d9d84ef9639
Antonio Abella <antonioabella@designiostecnologicos.com>
2018-04-05 20:32:53 -0400

    refactor: clean raw_test code

--- test/tags/raw_test.exs
+++ test/tags/raw_test.exs
@@ -1,31 +1,30 @@
 defmodule Liquid.RawTest do
   use ExUnit.Case
 
-  alias Liquid.Template, as: Template
+  alias Liquid.Template
 
   setup_all do
-    Liquid.start
+    Liquid.start()
     :ok
   end
 
   test :test_tag_in_raw do
-    assert_template_result "{% comment %} test {% endcomment %}",
-      "{% raw %}{% comment %} test {% endcomment %}{% endraw %}"
+    assert_result("{% comment %} test {% endcomment %}", "{% raw %}{% comment %} test {% endcomment %}{% endraw %}")
   end
 
   test :test_output_in_raw do
-    assert_template_result "{{ test }}", "{% raw %}{{ test }}{% endraw %}"
+    assert_result("{{ test }}", "{% raw %}{{ test }}{% endraw %}")
   end
 
   test :test_open_tag_in_raw do
-    assert_template_result " Foobar {% invalid ", "{% raw %} Foobar {% invalid {% endraw %}"
-    assert_template_result " Foobar invalid %} ", "{% raw %} Foobar invalid %} {% endraw %}"
-    assert_template_result " Foobar {{ invalid ", "{% raw %} Foobar {{ invalid {% endraw %}"
-    assert_template_result " Foobar invalid }} ", "{% raw %} Foobar invalid }} {% endraw %}"
-    assert_template_result " Foobar {% invalid {% {% endraw ", "{% raw %} Foobar {% invalid {% {% endraw {% endraw %}"
-    assert_template_result " Foobar {% {% {% ", "{% raw %} Foobar {% {% {% {% endraw %}"
-    assert_template_result " test {% raw %} {% endraw %}", "{% raw %} test {% raw %} {% {% endraw %}endraw %}"
-    assert_template_result " Foobar {{ invalid 1", "{% raw %} Foobar {{ invalid {% endraw %}{{ 1 }}"
+    assert_result(" Foobar {% invalid ", "{% raw %} Foobar {% invalid {% endraw %}")
+    assert_result(" Foobar invalid %} ", "{% raw %} Foobar invalid %} {% endraw %}")
+    assert_result(" Foobar {{ invalid ", "{% raw %} Foobar {{ invalid {% endraw %}")
+    assert_result(" Foobar invalid }} ", "{% raw %} Foobar invalid }} {% endraw %}")
+    assert_result(" Foobar {% invalid {% {% endraw ", "{% raw %} Foobar {% invalid {% {% endraw {% endraw %}")
+    assert_result(" Foobar {% {% {% ", "{% raw %} Foobar {% {% {% {% endraw %}")
+    assert_result(" test {% raw %} {% endraw %}", "{% raw %} test {% raw %} {% {% endraw %}endraw %}")
+    assert_result(" Foobar {{ invalid 1", "{% raw %} Foobar {{ invalid {% endraw %}{{ 1 }}")
   end
 
   # test :test_invalid_raw do
@@ -34,14 +33,8 @@ defmodule Liquid.RawTest do
   #   assert_match_syntax_error ~r/Valid syntax/, "{% raw } foo %}{% endraw %}"
   # end
 
-  defp assert_template_result(expected, markup) do
-    assert_result(expected, markup, %{})
-  end
-
-  defp assert_result(expected, markup, assigns) do
-    template = Template.parse(markup)
-
-    { :ok, result, _ } = Template.render(template, assigns)
+  defp assert_result(expected, markup, assigns \\ %{}) do
+    {:ok, result, _} = markup |> Template.parse() |> Template.render(assigns)
     assert result == expected
   end
 end
